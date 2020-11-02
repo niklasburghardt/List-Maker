d30be4fa1f62b129d05da73d1590e23a51094903
Niklas Burghardt <niklas.burghardt@web.de>
2020-11-02 09:41:18 +0100

    Implementing the ability to change the user's properties and finding them by the name

--- Interface/Input.java
+++ Interface/Input.java
@@ -1,6 +1,7 @@
 package Interface;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,21 +13,41 @@ public class Input {
 
 
     public void addUsers(){
-        Scanner scanner = new Scanner(System.in);
-        String[] current = new String[propNames.length];
-        mark: while(true){
-            for(int i = 0; i<propNames.length; i++){
-                System.out.print(propNames[i]+": ");
-                current[i] = scanner.nextLine();
-                //test
-                if(current[i].equals("skip")){
-                    break mark;
-                }
+        String[] current;
+        while(true){
+            current = inputProperties();
+            if(current[0].equals("skip")){
+                break;
             }
             people.add(new Properties(current[0], current[1], current[2], current[3], current[4]));
             System.out.println("USER ADDED SUCCESSFULLY...");
         }
-        scanner.close();
+    }
+
+    public void changeByProperty(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("NAME: ");
+        String[] name = scanner.nextLine().split(" ");
+        System.out.println(Arrays.toString(name));
+
+        for(Properties prop : people){
+            if(prop.firstName.equals(name[0]) && prop.name.equals(name[1])){
+                String[] current = inputProperties();
+                prop.changeProperties(current[0], current[1], current[2], current[3], current[4]);
+                System.out.println("USER CHANGED SUCCESSFULLY...");
+            }
+        }
+    }
+
+    public String[] inputProperties(){
+        Scanner scanner = new Scanner(System.in);
+        String[] current = new String[propNames.length];
+        for(int i = 0; i<propNames.length; i++){
+            System.out.print(propNames[i]+": ");
+            current[i] = scanner.nextLine();
+        }
+
+        return current;
     }
 
     void showList(){
@@ -40,6 +61,8 @@ public class Input {
     public static void main(String[] args){
         Input i = new Input();
         i.addUsers();
+        i.showList();
+        i.changeByProperty();
         i.showList();
     }
 }
--- Interface/Properties.java
+++ Interface/Properties.java
@@ -9,18 +9,14 @@ public class Properties{
     String birth;
 
     public Properties(String firstName, String name, String phone, String email, String birth){
+        changeProperties(firstName, name, phone, email, birth);
+    }
+
+    public void changeProperties(String firstName, String name, String phone, String email, String birth){
         this.firstName = firstName;
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.birth = birth;
+            this.name = name;
+            this.phone = phone;
+            this.email = email;
+            this.birth = birth;
     }
-
-    public void editByPropertie(String prop, String firstName, String name, String phone, String email, String birth){
-        Input in = new Input();
-        for(int i = 0; i<in.people.size(); i++){
-            
-        }
-    }
-
 }
